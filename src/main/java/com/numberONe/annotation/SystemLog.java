6aae8e7306cc48852a2ec0c4b4c14ea61c55a642
HUHAO <huhao@konyo.com.cn>
2018-05-08 18:05:33 +0800

    测试提交路径

--- src/main/java/com/numberONe/annotation/SystemLog.java
+++ src/main/java/com/numberONe/annotation/SystemLog.java
@@ -14,7 +14,7 @@ public  @interface SystemLog {
 	String module()  default "";  //模块名称 系统管理-用户管理－列表页面
 	String methods()  default "";  //新增用户
     String description()  default "";  //
-  
+	//测试一下提交路径
   
 }  
   
